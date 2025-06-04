3413269d2c28287662933864dc0e7a3448b6a1d4
HafsanaCA <hafsanaca@gmail.com>
2025-06-04 17:42:52 +0530

    Day 4

--- controllers/website_return_management.py
+++ controllers/website_return_management.py
@@ -1,38 +1,67 @@
-from odoo.addons.website.controllers import main
+# -*- coding: utf-8 -*-
 from datetime import datetime
 from odoo import http
 from odoo.http import request
-
+from odoo.addons.website.controllers import main
 
 class CustomerRegistration(main.Home):
 
-    @http.route('/sale_return', type='http', methods=['POST'], auth="public", website=True,
-                csrf=False)
+    @http.route('/sale_return', type='http', methods=['POST'], auth="user", website=True)
     def sale_return(self, **kwargs):
-        """Controller to create return order"""
-        product_id = request.env['product.product'].sudo().browse(int(kwargs['product']))
-        order = request.env['sale.order'].sudo().browse(int(kwargs['order_id']))
-        qty = kwargs['qty']
-        reason = kwargs['reason']
-        values = {
-            'partner_id': order.partner_id.id,
-            'sale_order': order.id,
-            'product_id': product_id.id,
-            'quantity': qty,
-            'reason': reason,
-            'user_id': request.env.uid,
-            'create_date': datetime.now(),
-        }
-        stock_picks = request.env['stock.picking'].search([('origin', '=', order.name)])
-        moves = stock_picks.mapped('move_ids_without_package').with_user(1).filtered(lambda p: p.product_id == product_id)
-        if moves:
-            moves = moves.sorted('product_uom_qty', reverse=True)
-            values.update({'state': 'draft'})
-            ret_order = request.env['sale.return'].with_user(1).create(values)
-            moves[0].picking_id.return_order = ret_order.id
-            moves[0].picking_id.return_order_picking = False
-        return request.redirect('/my/request-thank-you')
+        """Controller to create return orders for multiple products"""
+        try:
+            order_id = int(kwargs.get('order_id', 0))
+            order = request.env['sale.order'].sudo().browse(order_id)
 
-    @http.route('/my/request-thank-you', website=True, page=True, auth='public', csrf=False)
+            if not order or order.partner_id != request.env.user.partner_id:
+                return request.redirect('/my')
+
+            reason = kwargs.get('reason')
+            if not reason:
+                return request.redirect('/my/orders/%s' % order_id)
+
+            created = False
+            for key, value in kwargs.items():
+                if key.startswith('product_'):
+                    product_id = int(value)
+                    qty_key = f'qty_{product_id}'
+                    qty_val = float(kwargs.get(qty_key, 0))
+
+                    if qty_val <= 0:
+                        continue
+
+                    product = request.env['product.product'].sudo().browse(product_id)
+                    order_line = order.order_line.filtered(lambda l: l.product_id.id == product_id and l.qty_delivered >= qty_val)
+                    if not order_line:
+                        continue
+
+                    ret_order = request.env['sale.return'].sudo().create({
+                        'partner_id': order.partner_id.id,
+                        'sale_order': order.id,
+                        'product_id': product_id,
+                        'quantity': qty_val,
+                        'reason': reason,
+                        'user_id': request.env.uid,
+                        'create_date': datetime.now(),
+                        'state': 'draft',
+                    })
+                    created = True
+
+                    stock_picks = request.env['stock.picking'].search([('origin', '=', order.name)])
+                    moves = stock_picks.mapped('move_ids_without_package').filtered(lambda p: p.product_id.id == product_id)
+                    if moves:
+                        moves = moves.sorted('product_uom_qty', reverse=True)
+                        moves[0].picking_id.return_order = ret_order.id
+                        moves[0].picking_id.return_order_picking = False
+
+            if not created:
+                return request.redirect('/my/orders/%s' % order_id)
+
+            return request.redirect('/my/request-thank-you')
+
+        except Exception:
+            return request.redirect('/my/orders/%s' % kwargs.get('order_id', 0))
+
+    @http.route('/my/request-thank-you', type='http', auth='user', website=True)
     def maintenance_request_thanks(self):
         return request.render('product_return.customers_request_thank_page')
--- static/src/js/sale_return.js
+++ static/src/js/sale_return.js
@@ -1,30 +1,24 @@
 /** @odoo-module **/
+
 import publicWidget from "@web/legacy/js/public/public_widget";
 
 publicWidget.registry.return_order = publicWidget.Widget.extend({
-    selector: '#quote_content_return', // Updated selector
+    selector: '#hidden_box',
     events: {
         'click #hidden_box_btn': '_onHiddenBoxBtnClick',
         'change #product': '_onProductChange',
     },
-    /**
-     * @override
-     */
     start: function () {
         console.log("Return Order Widget Initialized");
         return this._super.apply(this, arguments);
     },
-
     _onHiddenBoxBtnClick: function (ev) {
-        var self = this;
         ev.preventDefault();
-        self.$('#hidden_box').modal('show');
+        this.$('#hidden_box').modal('show');
     },
-
     _onProductChange: function (ev) {
-        var self = this;
         var $product = $(ev.currentTarget);
-        var $submitButton = self.$('#submit');
+        var $submitButton = this.$('#submit');
         $submitButton.toggleClass('d-none', $product.val() === 'none');
     },
 });
